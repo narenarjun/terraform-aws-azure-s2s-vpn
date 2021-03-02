71e17cb96d2bd33b0624b6cd81bf63526237ee69
Narendran <narendev0610@gmail.com>
2021-03-02 13:47:54 +0530

    code for tunnel form azure to aws done

--- azure.tf
+++ azure.tf
@@ -55,10 +55,85 @@ resource "azurerm_virtual_network_gateway" "az-virtual-network-gateway" {
     subnet_id                     = azurerm_subnet.az-subnet-gateway.id
   }
 
-    ip_configuration {
+  ip_configuration {
     name                          = azurerm_public_ip.az-public-ip-2.name
     public_ip_address_id          = azurerm_public_ip.az-public-ip-2.id
     private_ip_address_allocation = "Dynamic"
     subnet_id                     = azurerm_subnet.az-subnet-gateway.id
   }
+}
+
+# 2 tunnels for aws connection1
+resource "azurerm_local_network_gateway" "local-network-gateway-1-tunnel1" {
+  name                = "local-network-gateway-1-tunnel1"
+  location            = azurerm_resource_group.az-resource-group.location
+  resource_group_name = azurerm_resource_group.az-resource-group.name
+  gateway_address     = aws_vpn_connection.vpn-connection-1.tunnel1_address
+  address_space       = [aws_vpc.vpc-main.cidr_block]
+}
+
+resource "azurerm_virtual_network_gateway_connection" "virtual-network-gateway-connection-1-tunnel1" {
+  name                       = "virtual-network-gateway-connection-1-tunnel1"
+  location                   = azurerm_resource_group.az-resource-group.location
+  resource_group_name        = azurerm_resource_group.az-resource-group.name
+  type                       = "IPSec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.az-virtual-network-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.local-network-gateway-1-tunnel1.id
+  shared_key                 = aws_vpn_connection.vpn-connection-1.tunnel1_preshared_key
+}
+
+resource "azurerm_local_network_gateway" "local-network-gateway-1-tunnel2" {
+  name                = "local-network-gateway-1-tunnel2"
+  location            = azurerm_resource_group.az-resource-group.location
+  resource_group_name = azurerm_resource_group.az-resource-group.name
+  gateway_address     = aws_vpn_connection.vpn-connection-1.tunnel2_address
+  address_space       = [aws_vpc.vpc-main.cidr_block]
+}
+
+resource "azurerm_virtual_network_gateway_connection" "virtual-network-gateway-connection-1-tunnel2" {
+  name                       = "virtual-network-gateway-connection-1-tunnel2"
+  location                   = azurerm_resource_group.az-resource-group.location
+  resource_group_name        = azurerm_resource_group.az-resource-group.name
+  type                       = "IPSec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.az-virtual-network-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.local-network-gateway-1-tunnel2.id
+  shared_key                 = aws_vpn_connection.vpn-connection-1.tunnel2_preshared_key
+}
+
+
+# 2 tunnels for aws connection2
+resource "azurerm_local_network_gateway" "local-network-gateway-2-tunnel1" {
+  name                = "local-network-gateway-2-tunnel1"
+  location            = azurerm_resource_group.az-resource-group.location
+  resource_group_name = azurerm_resource_group.az-resource-group.name
+  gateway_address     = aws_vpn_connection.vpn-connection-2.tunnel1_address
+  address_space       = [aws_vpc.vpc-main.cidr_block]
+}
+
+resource "azurerm_virtual_network_gateway_connection" "virtual-network-gateway-connection-2-tunnel1" {
+  name                       = "virtual-network-gateway-connection-2-tunnel1"
+  location                   = azurerm_resource_group.az-resource-group.location
+  resource_group_name        = azurerm_resource_group.az-resource-group.name
+  type                       = "IPSec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.az-virtual-network-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.local-network-gateway-2-tunnel1.id
+  shared_key                 = aws_vpn_connection.vpn-connection-2.tunnel1_preshared_key
+}
+
+resource "azurerm_local_network_gateway" "local-network-gateway-2-tunnel2" {
+  name                = "local-network-gateway-2-tunnel2"
+  location            = azurerm_resource_group.az-resource-group.location
+  resource_group_name = azurerm_resource_group.az-resource-group.name
+  gateway_address     = aws_vpn_connection.vpn-connection-2.tunnel2_address
+  address_space       = [aws_vpc.vpc-main.cidr_block]
+}
+
+resource "azurerm_virtual_network_gateway_connection" "virtual-network-gateway-connection-2-tunnel2" {
+  name                       = "virtual-network-gateway-connection-2-tunnel2"
+  location                   = azurerm_resource_group.az-resource-group.location
+  resource_group_name        = azurerm_resource_group.az-resource-group.name
+  type                       = "IPSec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.az-virtual-network-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.local-network-gateway-2-tunnel2.id
+  shared_key                 = aws_vpn_connection.vpn-connection-2.tunnel2_preshared_key
 }
